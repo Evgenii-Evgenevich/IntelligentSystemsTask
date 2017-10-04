90190963e19dd3901c5d2acbe125b9039c2a327d
Evgenii Evgenievich <eb_genius@mail.ru>
2017-10-04 18:36:30 +0300

    task6

---
+++ task5/Main.java
@@ -0,0 +1,8 @@
+package task5;
+
+/**
+ * Created by EE on 04.10.2017.
+ */
+
+public class Main {
+}
---
+++ task6/Main.java
@@ -0,0 +1,42 @@
+package task6;
+
+/**
+ * Created by EE on 04.10.2017.
+ */
+
+import java.io.*;
+
+public class Main {
+
+    public static int countChars(InputStreamReader reader, char c) throws IOException {
+        int count = 0;
+
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        String currentLine = "";
+
+        while ((currentLine = bufferedReader.readLine()) != null) {
+            for (int i = 0; i < currentLine.length(); ++i) {
+                if (currentLine.charAt(i) == c) {
+                    ++count;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String filepath = args[0];
+
+        char c = args[1].charAt(0);
+
+        InputStreamReader reader = new InputStreamReader(new FileInputStream(filepath), "UTF-8");
+
+        int count = Main.countChars(reader, c);
+
+        reader.close();
+
+        System.out.println(count);
+    }
+}
