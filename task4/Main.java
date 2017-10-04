b80c7c16745d87c72efd5eeb9d454406c5dbd9c0
Evgenii Evgenievich <eb_genius@mail.ru>
2017-10-04 18:10:19 +0300

    task4

--- task4/Main.java
+++ task4/Main.java
@@ -16,24 +16,36 @@ public class Main {
         StreamOfLinesScanner streamOfLinesScanner = null;
         StreamOfWordsScanner streamOfWordsScanner = null;
 
+        // test StreamOfDoublesScanner
+        if (false)
         {
             streamOfDoublesScanner = new StreamOfDoublesScanner(System.in);
             DoubleStream stream = streamOfDoublesScanner.stream();
             stream.limit(5).forEach(x -> System.out.println(x));
         }
 
+        // test StreamOfIntegersScanner
+        if (false)
         {
             streamOfIntegersScanner = new StreamOfIntegersScanner(System.in);
             IntStream stream = streamOfIntegersScanner.stream();
             stream.limit(5).forEach(x -> System.out.println(x));
         }
 
+        // test StreamOfLinesScanner
+        if (false)
         {
             streamOfLinesScanner = new StreamOfLinesScanner(System.in);
             Stream<String> stream = streamOfLinesScanner.stream();
             stream.limit(5).forEach(x -> System.out.println(x));
         }
+
+        // test StreamOfWordsScanner
+        if (true)
+        {
+            streamOfWordsScanner = new StreamOfWordsScanner(System.in);
+            Stream<String> stream = streamOfWordsScanner.stream();
+            stream.limit(5).forEach(x -> System.out.println(x));
+        }
     }
-
-
 }
--- task4/StreamOfWordsScanner.java
+++ task4/StreamOfWordsScanner.java
@@ -6,13 +6,46 @@ package task4;
 
 import java.io.InputStream;
 import java.util.Scanner;
+import java.text.BreakIterator;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class StreamOfWordsScanner {
 
     private Scanner scanner = null;
+    private BreakIterator wordFinder = null;
+    private String currentLine = null;
 
     public StreamOfWordsScanner(InputStream inputStream) {
         this.scanner = new Scanner(inputStream);
+        this.wordFinder = BreakIterator.getWordInstance();
+        this.currentLine = "";
+    }
+
+    private Supplier<String> supplier = () -> {
+        while (currentLine.length() == 0) {
+            this.currentLine = this.scanner.nextLine();
+        }
+
+        // find word
+        this.wordFinder.setText(this.currentLine);
+        int start = this.wordFinder.first();
+        int end = this.wordFinder.next();
+
+        // get word from line
+        String returnValue = this.currentLine.substring(start, end);
+
+        if (end != this.currentLine.length()) {
+            ++end;
+        }
+
+        // substring line
+        this.currentLine = this.currentLine.substring(end);
+
+        return returnValue;
+    };
+
+    public Stream<String> stream() {
+        return Stream.generate(this.supplier);
     }
 }
