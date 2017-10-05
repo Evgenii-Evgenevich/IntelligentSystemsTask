d05e21478532f1fae83f2d3c9f235906649b1e77
Evgenii Evgenievich <eb_genius@mail.ru>
2017-10-06 02:21:18 +0300

    task7

--- task7/Main.java
+++ task7/Main.java
@@ -4,6 +4,43 @@ package task7;
  * Created by EE on 04.10.2017.
  */
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+    public static void main(String[] args) throws IOException {
+        String filename = args[0];
+        int numLines = Integer.parseInt(args[1]);
+
+        List<String> linesList = new ArrayList<String>(numLines);
+
+        File file = new File(filename);
+        long length = file.length();
+
+        RandomAccessFile raf = new RandomAccessFile(filename, "r");
+
+        while (numLines != 0) {
+            --numLines;
+
+            long i = (long)(Math.random() * length);
+
+            raf.seek(i);
+
+            while (i != 0 && raf.read() != '\n') {
+                --i;
+
+                raf.seek(i);
+            }
+
+            linesList.add(raf.readLine());
+        }
+
+        raf.close();
+
+        linesList.forEach(System.out::println);
+    }
 }
